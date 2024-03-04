837318537c0d231784022e654d5d1ab656a8bba2
MuratBuyukk <murat_buyuk_1999@hotmail.com>
2024-03-04 21:29:50 +0300

    14. Longest Common Prefix .. solved!

---
+++ 14_Longest_Common_Prefix/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Longest_Common_Prefix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] srtArray = { "abab", "aba", "abc" };
+            Console.WriteLine(LongestCommonPrefix(srtArray));
+            Console.ReadLine();
+        }
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            Array.Sort(strs, (x, y) => x.Length.CompareTo(y.Length));
+            string answer = "";
+            bool isOk = false;
+            if (strs.Length == 1)
+                return answer += strs[0];
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                char temp = strs[0][i];
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (strs[j][i] == temp)
+                    {
+                        isOk = true;
+                    }
+                    else
+                    {
+                        isOk = false;
+                        break;
+                    }
+                }
+                if (isOk)
+                {
+                    answer += temp;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return answer;
+        }
+    }
+}
