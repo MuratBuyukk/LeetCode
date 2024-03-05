c9cbe0745816efd0f63279cedbc7ceb3f3085f7b
MuratBuyukk <murat_buyuk_1999@hotmail.com>
2024-03-05 16:21:46 +0300

    28. Find the Index of the First Occurrence in a String, Solved!

---
+++ 28_Find_IndexOfThe_FirstOccurence/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_Find_IndexOfThe_FirstOccurence
+{
+//Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+//Example 1:
+//Input: haystack = "sadbutsad", needle = "sad"
+//Output: 0
+//Explanation: "sad" occurs at index 0 and 6.
+//The first occurrence is at index 0, so we return 0.
+//Example 2:
+//Input: haystack = "leetcode", needle = "leeto"
+//Output: -1
+//Explanation: "leeto" did not occur in "leetcode", so we return -1.
+//Constraints:
+//1 <= haystack.length, needle.length <= 104
+//haystack and needle consist of only lowercase English characters.
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "leetcode";
+            string s2 = "leeto";
+            Console.WriteLine(StrStr(s1,s2));
+            Console.ReadLine();
+        }
+        public static int StrStr(string haystack, string needle)
+        {
+            return haystack.IndexOf(needle);
+        }
+
+    }
+}
