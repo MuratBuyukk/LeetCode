dc0c5950c0ef91a56f11bac30d2bcedcdc60c1ec
MuratBuyukk <murat_buyuk_1999@hotmail.com>
2024-03-28 20:58:14 +0300

    202. Happy Number Solved..!

---
+++ 202_HappyNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _202_HappyNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 19;
+            Console.WriteLine(IsHappy(n));
+            Console.ReadLine();
+        }
+
+        public static bool IsHappy(int n)
+        {
+            HashSet<int> oldResults = new HashSet<int>();
+            while (oldResults.Add(n))
+            {
+                int result = 0;
+                while (n > 0)
+                {
+                    result += (int)Math.Pow(n % 10, 2.0);
+                    n = n / 10;
+                }
+                n = result;
+            }
+            if (n == 1) return true;
+       
+            return false;
+        }
+    }
+}
