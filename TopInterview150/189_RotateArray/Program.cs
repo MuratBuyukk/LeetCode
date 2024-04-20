4ca2e3fef24e1d9eb5a8e508d92bbcfe68ef462e
MuratBuyukk <murat_buyuk_1999@hotmail.com>
2024-04-20 22:21:33 +0300

    189. Rotate Array Solved!

---
+++ TopInterview150/189_RotateArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _189_RotateArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] array = { -1 };
+            int k = 3;
+            solution.Rotate(array, k);
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            k %= nums.Length;
+            Reverse(nums, 0, nums.Length - 1);
+            Reverse(nums, 0, k - 1);
+            Reverse(nums, k, nums.Length - 1);
+        }
+
+        public void Reverse(int[] nums,int start, int end) {
+
+            while (start<end) 
+            {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
