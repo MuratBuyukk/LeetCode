86fc499ba1755a302d84714a405b655c39809c8b
MuratBuyukk <murat_buyuk_1999@hotmail.com>
2024-10-15 21:52:53 +0300

    Jump Game II Solved..!

---
+++ TopInterview150/45_Jump_Game_II/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] example1 = {2, 3, 1, 1, 4 };
+        int[] example2 = {2, 3, 0, 1, 4 };
+
+
+        Console.WriteLine(Solution.Jump(example1) == 2 ? "Example 1 Passed..!" : "Example 1 Failed..!");
+        Console.WriteLine(Solution.Jump(example2) == 2 ? "Example 2 Passed..!" : "Example 2 Failed..!");
+
+    }
+}
---
+++ TopInterview150/45_Jump_Game_II/Solution.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public static int Jump(int[] nums)
+    {
+        int jumpCount = 0;
+        int jumpRange = 0;
+        int maxRange = 0;
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            maxRange = Math.Max(maxRange, i + nums[i]);
+            if (jumpRange == i)
+            {
+                jumpCount++;
+                jumpRange = maxRange;
+            }
+        }
+        return jumpCount;
+    }
+}
