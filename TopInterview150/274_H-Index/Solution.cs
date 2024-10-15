e322022e3ecbb47b7f7e244a58aa5438941b5e25
MuratBuyukk <murat_buyuk_1999@hotmail.com>
2024-10-15 22:56:21 +0300

    274. H-Index

---
+++ TopInterview150/274_H-Index/Solution.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public static int HIndex(int[] citations)
+    {
+        var sortedCitations = citations.OrderByDescending(c => c).ToArray();
+
+        int hIndex = 0;
+        for (int i = 0; i < sortedCitations.Length; i++)
+        {
+            if (sortedCitations[i] >= i + 1) hIndex++;
+            else break;
+        }
+        return hIndex;
+    }
+}
